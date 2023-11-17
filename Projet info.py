5941b0319517d63d8c7094b6b1b3a2b11ac4df52
jordaaaaan <151128282+jordaaaaan@users.noreply.github.com>
2023-11-17 09:35:41 +0100

    Update Projet info.py

--- Projet info.py
+++ Projet info.py
@@ -6,3 +6,8 @@ def display_map(m, d):
 mapp=[[0,0,1],[0,1,1],[0,1,0]]
 legende={0:' ',1:'#'}
 display_map(mapp,legende)
+def create_perso(pos):
+    P={"char":"o","x":pos[0],"y":pos[1]}
+    return P
+create_perso((0,0))  
+
