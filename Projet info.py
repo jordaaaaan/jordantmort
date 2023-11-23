65f2f358e86f9d10c774cead70408f4532ba20c0
jordantmort <151127401+jordantmort@users.noreply.github.com>
2023-11-23 21:39:45 +0100

    Update Projet info.py

--- Projet info.py
+++ Projet info.py
@@ -1,3 +1,4 @@
+
 def display_map(m, d):
     for row in m:
         for x in row:
@@ -10,18 +11,17 @@ def create_perso(pos):
     P={"char":"o","x":pos[0],"y":pos[1]}
     return P
 create_perso((0,0))  
-def display_map_and_char(m,p,d):
-    for i in range(len(m)):
-        for j in range(len(m[0])):
-            if i==p[0] and j==p[1]:
-                print('o',end='')
-            else:
-                print(d[m[i][j]],end=' ')
+def display_map_and_char(m, d, p):
+    map2= [ligne.copy() for ligne in m]
+    map2[p["y"]][p["x"]] = p["char"]
+    for ligne in map2:
+        for x in ligne:
+            print(d[x], end=' ')
         print()
-mapp=[[0,0,1],[0,1,1],[0,1,0]]
-legende={0:' ',1:'#'}
-display_map_and_char(mapp,p1,legende)
-
+mapp = [[0, 0, 1], [0, 1, 1], [0, 1, 0]]
+legende = {0: ' ', 1: '#'}
+perso = create_perso((0, 0))
+display_map_and_char(mapp, legende, perso)
 def update_p(lettre, p,m):
     if lettre.lower() in ["z", "q", "s", "d"]:
         posi = p.get("x""y", (0, 0))
@@ -48,15 +48,4 @@ while True:
         break
     else:
         update_p(direc,p,m)
-    
-    
-    
-
-
- 
-
-
-
- 
-
     
