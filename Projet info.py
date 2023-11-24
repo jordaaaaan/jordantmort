9b2b3c7ea46828b45a69f2d277ef13825f6a7f55
jordaaaaan <151128282+jordaaaaan@users.noreply.github.com>
2023-11-24 08:30:23 +0100

    Update Projet info.py

--- Projet info.py
+++ Projet info.py
@@ -22,30 +22,4 @@ mapp = [[0, 0, 1], [0, 1, 1], [0, 1, 0]]
 legende = {0: ' ', 1: '#'}
 perso = create_perso((0, 0))
 display_map_and_char(mapp, legende, perso)
-def update_p(lettre, p,m):
-    if lettre.lower() in ["z", "q", "s", "d"]:
-        posi = p.get("x""y", (0, 0))
-        x, y = posi
-        if lettre.lower() == "z" and x-1>=0:  
-            posf= (x - 1, y)
-        elif lettre.lower() == "q":
-            posf = (x + 1, y)
-        elif lettre.lower() == "s":
-            posf = (x, y + 1)
-        elif lettre.lower() == "d":
-            posf = (x, y - 1)
-        p["position"] =posf
-    else:
-        print("direction innaccessible utilisez 'z' 'q'  's' ou 'd' .")
-perso={"position":(1,1)}
-m=[[0,0,1],[0,1,1],[0,1,0]]
-dico = {"joueur": "o"}
 
-while True:
-    display_map_and_char(m,perso,dico)
-    direc= input("Choisissez une direction (z, q, s, d) ou 'a' pour quitter : ")
-    if direc.lower()=='a':
-        break
-    else:
-        update_p(direc,p,m)
-    
