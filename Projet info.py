8f6fe2fed361e44d43de4b4a5fe31f75dc7c17d1
jordantmort <151127401+jordantmort@users.noreply.github.com>
2023-11-26 22:01:17 +0100

    Update Projet info.py

--- Projet info.py
+++ Projet info.py
@@ -11,6 +11,18 @@ def create_perso(pos):
     P={"char":"o","x":pos[0],"y":pos[1]}
     return P
 create_perso((0,0))  
+
+def display_map_and_char(m, d, p):
+    copy_map= [i.copy() for i in m]
+    copy_map[p["y"]][p["x"]] = p["char"]
+    for i in copy_map:
+        for j in i:
+            print(j, end=' ')
+        print()
+mapp = [[0, 0, 1], [0, 1, 1], [0, 1, 0]]
+legende = {0:'',1:'#'}
+perso = create_perso((0, 0))
+display_map_and_char(mapp, legende, perso)
 def display_map_and_char(m, d, p):
     map2= [ligne.copy() for ligne in m]
     map2[p["y"]][p["x"]] = p["char"]
