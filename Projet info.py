334d5addbf7da1293c6f9a072e029f5f40050519
jordantmort <151127401+jordantmort@users.noreply.github.com>
2023-11-30 20:57:00 +0100

    Update Projet info.py

--- Projet info.py
+++ Projet info.py
@@ -125,10 +125,11 @@ def display_map_char_and_objects(m, d, p, objects):
             print(d[cell], end=' ')
         print()
 
+p=random.randint(2,5)
 mapp = [[0, 0, 1], [0, 1, 1], [0, 0, 0],[0,0,1]]
 legende = {0: ' ', 1: '#', 'o': 'o', '.': '.'}
 perso = create_perso((0, 2))
-objects = create_objects(3, mapp)
+objects = create_objects(p, mapp)
 
 
 
@@ -173,6 +174,7 @@ while True:
     direction = input("Quel déplacement ? ")
     update_p(direction, perso, mapp)
     update_objects(perso, objects)
+
 
 
 
